0e822d69987bf94c0ce4a5f73e05e7ec236454e1
Yoo.DH <dhlovehd@naver.com>
2024-06-28 16:29:58 +0900

    commit_main

---
+++ toy_project/src/styles/styledLogin.jsx
@@ -0,0 +1,74 @@
+import { styled, css } from "styled-components";
+
+export const Container = styled.div`
+  width: 390px;
+  height: 844px;
+  margin: 0 auto;
+  background: #121212;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  overflow: hidden;
+`;
+
+export const Group = styled.div`
+  width: 157px;
+  height: 118px;
+`;
+
+export const Ellipse549 = styled.div`
+  width: 206px;
+  height: 205px;
+  background-color: #a259ff;
+  filter: blur(159.68670654296875px);
+  margin-top: -120px;
+`;
+
+export const InputContainer = styled.div`
+  width: 328px;
+  position: relative;
+  margin-bottom: 16px;
+`;
+
+export const InputLabel = styled.div`
+  color: #fff;
+  font-family: Barlow;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160.5%;
+  margin: 7px;
+`;
+
+export const UserInput = styled.input`
+  width: 313px;
+  height: 32px;
+  border-radius: 8px;
+  border: 1px solid #a259ff;
+  background: #121212;
+  color: rgba(255, 255, 255, 0.5);
+  font-family: Barlow;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160.5%;
+  padding-left: 10px;
+`;
+
+export const Complete = styled.button`
+  width: 328px;
+  height: 34px;
+  border-radius: 8px;
+  border: 1px solid #a259ff;
+  background: #a259ff;
+  color: #fff;
+  text-align: center;
+  font-family: Barlow;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160.5%;
+  cursor: pointer;
+`;
